2d17bdc4520bfc474cc9bc827eeca4353326d90f
Kevin Dai <kevindai@email.unc.edu>
2023-05-07 02:17:33 -0400

    sub15

--- cli.js
+++ cli.js
@@ -16,6 +16,7 @@ if (args.h) {
 	console.log("	-z		    Time zone: uses tz.guess() from moment-timezone by default.");
 	console.log("	-d 0-6		Day to retrieve weather: 0 is today; defaults to 1.");
 	console.log("	-j		    Echo pretty JSON from open-meteo API and exit.");
+	process.exit(0);
 }
 
 const timezone = args.z || moment.tz.guess();
